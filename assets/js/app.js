948c1ee3675655d12374dcb2c22964773fe2482d
ValeOlivares <valentinaolivares85@gmail.com>
2017-12-14 14:56:44 -0300

    agregando filtro

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,27 @@
-$(function(){
-   setTimeout(function() {
-      $('#splash').fadeOut(500);
-   }, 3000);
-
-});
-//volver al inicio, revisar no funcionaa
-/*$('#miboton').click(function({
-	location.reload();
-});*/  
-
 $(document).ready(function(){
+	$(function(){
+	   setTimeout(function() {
+	      $('#splash').fadeOut(500);
+	   }, 3000);
+
+	});
+
     $('.zoom').hover(function() {
         $(this).addClass('transition');
     }, function() {
         $(this).removeClass('transition');
     });
+
+     $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myList div").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
 });
+
+
+ //volver al inicio, revisar no funcionaa
+/*$('#miboton').click(function({
+	location.reload();
+});*/ 
